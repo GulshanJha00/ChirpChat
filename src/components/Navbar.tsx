c0ce7d0e37cadfb7a8bafc3506919f4d0a1957b2
GulshanJha00 <gk4763549@gmail.com>
2025-01-09 21:40:01 +0530

    icon changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='h-20 w-full bg-'>
+      <Image
+      src={"/logo2.png"}
+      height={50}
+      width={50}
+      alt='chirpchat'
+      />
     </div>
   )
 }
